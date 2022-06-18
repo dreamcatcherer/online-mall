a776e08537c5b464212882fa4fc79800895e9617
YuelinCui <yuelincui101@gmail.com>
2022-06-18 14:04:07 -0700

    removeParent

--- mall-product/src/main/java/com/yuelinc/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/yuelinc/mall/product/MallProductApplication.java
@@ -1,8 +1,10 @@
 package com.yuelinc.mall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.yuelinc.mall.product.dao")
 @SpringBootApplication
 public class MallProductApplication {
 
--- mall-product/src/test/java/com/yuelinc/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/yuelinc/mall/product/MallProductApplicationTests.java
@@ -1,13 +1,24 @@
 package com.yuelinc.mall.product;
 
+import com.yuelinc.mall.product.entity.BrandEntity;
+import com.yuelinc.mall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setDescript("hello");
+        brandEntity.setName("华为");
+        brandService.save(brandEntity);
+        System.out.println("保存成功");
     }
 
 }
